9b5c0b1cf397b9fabd31f5b30702a0e7654528c1
afe1 <2279948211@qq.com>
2023-05-14 15:25:07 +0800

    demo

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -5,7 +5,7 @@ import { getPosts } from './theme/serverUtils'
 const pageSize = 10
 
 export default defineConfig({
-    title: 'vitepress',
+    title: 'afe1',
     base: '/',
     cacheDir: './node_modules/vitepress_cache',
     description: 'vitepress,blog,blog-theme',
---
+++ posts/test.md
@@ -0,0 +1,15 @@
+---
+date: 2023-05-14
+title: demo
+tags:
+- vitpress
+- demo
+description: 尝试markdown语法
+---
+# 1
+## 2
+2
+
+## 3
+3
+<Comment />
